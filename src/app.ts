b686c9cb762e2495b14a4d738beb55e14543ef25
mahdimortuza <mahdi.webx@gmail.com>
2024-05-13 06:08:39 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use(cors({ origin: 'https://ocean-catch-client.vercel.app' }));
 
 app.use('/api/v1', router);
 
